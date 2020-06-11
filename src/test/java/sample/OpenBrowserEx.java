3ade42e50780a7c74213a9b44d6f4ae2540dfdf0
yvskvarma <yvskvarma@gmail.com>
2020-06-11 11:33:30 +0530

    update driver manager

--- src/test/java/sample/OpenBrowserEx.java
+++ src/test/java/sample/OpenBrowserEx.java
@@ -4,20 +4,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class OpenBrowserEx {
 	@Test
 	public void f() {
 
+		WebDriverManager.chromedriver().setup();
+
 		WebDriver driver = new ChromeDriver();
 
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Varma\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Varma\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
 
 		driver.get("http://www.google.com");
 
 		String poorna = driver.getTitle();
 
 		System.out.println("Page Title :" + poorna);
+		
+		driver.close();
 
 	}
 }
